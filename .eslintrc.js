a2dccb5eb01a594811a31ccafb42350f6f043f2d
tho.vo <votantho93@gmail.com>
2019-01-26 11:50:34 +0700

    add burger ingredients dynamic and config pretier intergrate with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['prettier'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/prop-types': 0,
@@ -7,9 +8,15 @@ module.exports = {
     'no-use-before-define': 0,
     'react/destructuring-assignment': 0,
     'no-console': 0,
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+    'no-unused-vars': 'warn'
   },
   env: {
     browser: true,
-    node: true,
+    node: true
   },
+  parser: 'babel-eslint'
 };
+// https://github.com/babel/babel-eslint/issues/312
+// https://medium.com/quick-code/how-to-integrate-eslint-prettier-in-react-6efbd206d5c4
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = () => (
-  <div className={classes.Burger}>
-    <BurgerIngredient type="bread-top" />
-    <BurgerIngredient type="cheese" />
-    <BurgerIngredient type="meat" />
-    <BurgerIngredient type="bread-bottom" />
-  </div>
-);
+const burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey =>
+    [Array(props.ingredients[igKey])].map((_, i) => (
+      <BurgerIngredient type={igKey} />
+    ))
+  );
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
 export default burger;
